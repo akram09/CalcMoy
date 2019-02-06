d936232ed1dea006a3710bf69e9b73306ed59034
akram09 <a.boutouchent@esi-sba.dz>
2019-02-06 23:23:31 +0100

    ProvideUserInfo added the facultytype gestion

--- domain/src/main/java/oxxy/kero/roiaculte/team7/domain/models/FacultyType.kt
+++ domain/src/main/java/oxxy/kero/roiaculte/team7/domain/models/FacultyType.kt
@@ -1,5 +1,5 @@
 package oxxy.kero.roiaculte.team7.domain.models
 
 enum class FacultyType {
-    LETTRE,SCIENCE,MATH,TM_CIVIL,TM_ELECT,TM_MECAN,TM_METH,PHILO,LANGUE
+    LETTRE,SCIENCE,MATH,TM_CIVIL,TM_ELECT,TM_MECAN,TM_METH,PHILO,LANGUE, GESTION
 }
